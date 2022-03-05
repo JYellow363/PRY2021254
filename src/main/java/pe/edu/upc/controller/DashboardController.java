0b24039c78ce4a8032e183ce58a6c23aead3dfd5
Jimena Alexandra Ruiz Cerna <jyellow363@gmail.com>
2022-03-04 19:36:36 -0500

    fix

--- src/main/java/pe/edu/upc/controller/DashboardController.java
+++ src/main/java/pe/edu/upc/controller/DashboardController.java
@@ -5,33 +5,36 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import pe.edu.upc.dto.LevelRecordDto;
 import pe.edu.upc.service.ILevelRecordService;
 
-@Controller
-@RequestMapping("/dashboard")
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/dashboard")
 public class DashboardController {
 	@Autowired
 	private ILevelRecordService levelRecordService;
-	
+
 	// Monitoring by category
 	@GetMapping(path = "/getDashboardCategory", produces = "application/json")
 	public ResponseEntity<?> getDashboardCategory(@RequestParam int idChildren) {
 		List<LevelRecordDto> levelRecords = levelRecordService.listByChildrenForCategory(idChildren);
 		return ResponseEntity.ok(levelRecords);
 	}
-	
+
 	// Monitoring by topic
 	@GetMapping(path = "/getDashboardTopic", produces = "application/json")
 	public ResponseEntity<?> getDashboardTopic(@RequestParam int idChildren, @RequestParam int idCategory) {
 		List<LevelRecordDto> levelRecords = levelRecordService.listByChildrenForTopic(idChildren, idCategory);
 		return ResponseEntity.ok(levelRecords);
 	}
-	
+
 	// Monitoring by level
 	@GetMapping(path = "/getDashboardLevel", produces = "application/json")
 	public ResponseEntity<?> getDashboardLevel(@RequestParam int idChildren, @RequestParam int idTopic) {
