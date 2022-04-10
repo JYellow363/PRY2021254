b8118905047f13f0edf19e4bb18d3f2c4b3c0ea9
Jimena Alexandra Ruiz Cerna <jyellow363@gmail.com>
2022-04-10 11:03:26 -0500

    transactional

--- src/main/java/pe/edu/upc/service/impl/PaymentServiceImpl.java
+++ src/main/java/pe/edu/upc/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package pe.edu.upc.service.impl;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,7 @@ public class PaymentServiceImpl implements IPaymentService {
 	@Autowired
 	private IGuardianRepository guardianRepository;
 
+	@Transactional
 	@Override
 	public int payPremium(PaymentDto payment) {
 		 int result = ValidatePayment.validate(payment);
